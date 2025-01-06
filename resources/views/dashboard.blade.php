0de92bc017084736d0d180639fd18b46f9f9d893
gelvis <gyauelvis@gmail.com>
2025-01-06 13:10:58 +0000

    requested dashboard graphs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,120 +17,6 @@
         })
     })
 
-    const monthOverview = [{
-        month: 'January',
-        donations: 1000,
-        funeralWages: 2000,
-        communityMembers: 1000,
-        donors: 500
-    }, {
-        month: 'February',
-        donations: 1500,
-        funeralWages: 2500,
-        communityMembers: 1200,
-        donors: 600
-    }, {
-        month: 'March',
-        donations: 2000,
-        funeralWages: 3000,
-        communityMembers: 1500,
-        donors: 700
-    }, {
-        month: 'April',
-        donations: 2500,
-        funeralWages: 3500,
-        communityMembers: 2000,
-        donors: 800
-    }]
-
-    const yearlyOverview = [{
-        year: 2022,
-        donations: 25000,
-        funeralWages: 35000,
-        communityMembers: 5000,
-        donors: 2500
-    }, {
-        year: 2023,
-        donations: 45000,
-        funeralWages: 55000,
-        communityMembers: 8000,
-        donors: 4000
-    }, {
-        year: 2023,
-        donations: 45000,
-        funeralWages: 55000,
-        communityMembers: 8000,
-        donors: 4000
-    }, {
-        year: 2024,
-        donations: 72000,
-        funeralWages: 85000,
-        communityMembers: 12000,
-        donors: 6000
-    }];
-
-    const populateYearlyOverview = (isDonor) => {
-        const monthlyOverview = document.getElementById('yearly-overview');
-        const overviewTitle = document.getElementById('year-overview-title');
-        if (isDonor) {
-            overviewTitle.innerHTML = 'Yearly Donations';
-        } else {
-            overviewTitle.innerHTML = 'Yearly Contribution';
-        }
-        monthlyOverview.innerHTML = '';
-        yearlyOverview.forEach(year => {
-            const li = document.createElement('li');
-            li.className = 'py-4 hover:bg-gray-50 transition-colors duration-200';
-            li.innerHTML = `<div class="flex items-center justify-between">
-            <div>
-                <p class="text-sm font-medium text-gray-900 mb-1">
-                    ${year.year}
-                </p>
-                <p class="text-sm text-gray-500">
-                    ${!isDonor ? year.communityMembers : year.donors}
-                </p>
-            </div>
-            <div class="text-sm font-semibold text-gray-800">
-                GH₵ ${!isDonor ? year.funeralWages : year.donations}
-            </div>
-        </div>`
-            monthlyOverview.appendChild(li);
-        });
-    }
-
-    populateYearlyOverview(false);
-
-    const populateMonthlyOverview = (isDonor) => {
-        const monthlyOverview = document.getElementById('monthly-overview');
-        const overviewTitle = document.getElementById('overview-title');
-        if (isDonor) {
-            overviewTitle.innerHTML = 'Monthly Donations';
-        } else {
-            overviewTitle.innerHTML = 'Monthly Contribution';
-        }
-        monthlyOverview.innerHTML = '';
-        monthOverview.forEach(month => {
-            const li = document.createElement('li');
-            li.className = 'py-4 hover:bg-gray-50 transition-colors duration-200';
-            li.innerHTML = `<div class="flex items-center justify-between">
-                <div>
-                    <p class="text-sm font-medium text-gray-900 mb-1">
-                        ${month.month}
-                    </p>
-                    <p class="text-sm text-gray-500">
-                        ${!isDonor ? month.communityMembers : month.donors}
-                    </p>
-                </div>
-                <div class="text-sm font-semibold text-gray-800">
-                    GH₵ ${!isDonor ? month.funeralWages : month.donations}
-                </div>
-            </div>`
-            monthlyOverview.appendChild(li);
-        });
-    }
-
-    populateMonthlyOverview(false);
-
     document.querySelectorAll('#monthly-drop-down-icon').forEach(icon => {
         icon.addEventListener('click', function() {
             icon.classList.add('rotate-180');
@@ -144,4 +30,297 @@
             dropDown.classList.remove('rotate-180');
         })
     })
+
+
+    const donationVlevyChartData = {
+        series: [{
+                name: "Donations",
+                data: [1432, 1567, 1389, 1478, 1523, 1445, 1298, 1534, 1476, 1390, 1512, 1467],
+                color: "#C81E1E",
+            },
+            {
+                name: "Levy",
+                data: [892, 1243, 567, 934, 1532, 789, 1345, 645, 1123, 876, 1654, 432],
+                color: "#000",
+            }
+        ],
+        chart: {
+            height: 300,
+            maxWidth: "100%",
+            type: "area",
+            dropShadow: {
+                enabled: false,
+            },
+            toolbar: {
+                show: false,
+            },
+        },
+        tooltip: {
+            enabled: true,
+            x: {
+                show: false,
+            },
+        },
+        legend: {
+            show: true
+        },
+        fill: {
+            type: "gradient",
+            gradient: {
+                opacityFrom: 0.55,
+                opacityTo: 0,
+                shade: "#1C64F2",
+                gradientToColors: ["#1C64F2"],
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        stroke: {
+            width: 6,
+        },
+        grid: {
+            show: false,
+            strokeDashArray: 4,
+            padding: {
+                left: 2,
+                right: 2,
+                top: 0
+            },
+        },
+        xaxis: {
+            categories: [
+                'Jan',
+                'Feb',
+                'Mar',
+                'Apr',
+                'May',
+                'Jun',
+                'Jul',
+                'Aug',
+                'Sep',
+                'Oct',
+                'Nov',
+                'Dec',
+            ],
+            labels: {
+                show: false,
+            },
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: true,
+            },
+        },
+        yaxis: {
+            show: false,
+            labels: {
+                formatter: function(value) {
+                    return 'GH₵' + value;
+                }
+            }
+        },
+    }
+
+    const donutData = () => {
+        return {
+            series: [1, 2],
+            colors: ['#C81E1E', '#000000'],
+            chart: {
+                height: 300,
+                width: "100%",
+                type: "donut",
+            },
+            stroke: {
+                colors: ["transparent"],
+                lineCap: "",
+            },
+            plotOptions: {
+                pie: {
+                    donut: {
+                        labels: {
+                            show: true,
+                            name: {
+                                show: true,
+                                offsetY: 20,
+                            },
+                            total: {
+                                showAlways: true,
+                                show: true,
+                                label: "Total Amount",
+                                formatter: function(w) {
+                                    const sum = w.globals.seriesTotals.reduce((a, b) => {
+                                        return a + b
+                                    }, 0)
+                                    return 'GH₵' + sum
+                                },
+                            },
+                            value: {
+                                show: true,
+                                offsetY: -20,
+                                formatter: function(value) {
+                                    return value
+                                },
+                            },
+                        },
+                        size: "80%",
+                    },
+                },
+            },
+            grid: {
+                padding: {
+                    top: -2,
+                },
+            },
+            labels: ["Male", "Female"],
+            dataLabels: {
+                enabled: false,
+            },
+            legend: {
+                position: "bottom",
+            },
+            yaxis: {
+                labels: {
+                    formatter: function(value) {
+                        return value
+                    },
+                },
+            },
+            xaxis: {
+                labels: {
+                    formatter: function(value) {
+                        return value
+                    },
+                },
+                axisTicks: {
+                    show: false,
+                },
+                axisBorder: {
+                    show: false,
+                },
+            },
+        }
+    }
+
+    const lineChartData = {
+        series: [{
+                name: 'Male',
+                group: 'male',
+                data: [48000, 50000, 40000]
+            },
+            {
+                name: 'Female',
+                group: 'female',
+                data: [20000, 40000, 25000]
+            }
+        ],
+        chart: {
+            type: 'bar',
+            height: 400,
+            stacked: false,
+            width: "100%",
+            background: 'transparent',
+            toolbar: {
+                show: false
+            }
+        },
+        stroke: {
+            width: 0
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: (val) => {
+                return `${(val / 1000).toFixed(1)}K`
+            },
+            style: {
+                fontSize: '12px',
+            }
+        },
+        plotOptions: {
+            bar: {
+                horizontal: true,
+                columnWidth: "60%", 
+                distributed: false,
+                dataLabels: {
+                    position: "center"
+                },
+            },
+        },
+        xaxis: {
+            categories: ['18-30', '31-45', '46+'],
+            labels: {
+                formatter: (val) => {
+                    return `${(val / 1000).toFixed(1)}K`
+                },
+                style: {
+                    fontSize: '12px',
+                }
+            },
+            axisBorder: {
+                show: false
+            },
+            axisTicks: {
+                show: false
+            }
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    fontSize: '12px',
+                }
+            }
+        },
+        grid: {
+            borderColor: '#f1f1f1',
+            xaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false
+                }
+            }
+        },
+        fill: {
+            opacity: 1
+        },
+        colors: ['#000', '#C81E1E'], 
+
+        legend: {
+            position: 'bottom',
+            horizontalAlign: 'center',
+            fontSize: '13px',
+            markers:{
+                shape: 'circle',
+            }
+        }
+    }
+
+
+
+    window.addEventListener('load', function() {
+        if (document.getElementById("labels-chart") && typeof ApexCharts !== 'undefined') {
+
+            let chart = new ApexCharts(document.getElementById("labels-chart"), donationVlevyChartData);
+            chart.render();
+
+            chart = new ApexCharts(document.getElementById("donut-chart"), donutData());
+            chart.render();
+
+            chart = new ApexCharts(document.getElementById("line-chart"), lineChartData);
+            chart.render();
+        }
+
+        const getAllSpans = document.querySelectorAll("span");
+        
+        getAllSpans.forEach(span => {
+            if(span.classList.contains('apexcharts-legend-marker')){
+                span.style.borderRadius = '999px';
+            }
+        });
+
+    });
 </script>
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -501,5 +501,183 @@
                 </div>
             </div>
         @endif
+
+        <div class="grid grid-cols-3 gap-6 py-10">
+            <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800">
+                <div class="flex justify-between p-4 md:p-6 pb-0 md:pb-0">
+                    <div>
+                        <h5 class="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">GH₵12,423</h5>
+                        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Levy This Month</p>
+                    </div>
+                    <div
+                        class="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
+                        23%
+                        <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 10 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4" />
+                        </svg>
+                    </div>
+                </div>
+                <div id="labels-chart" class="px-2.5"></div>
+                <div
+                    class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between mt-5 p-4 md:p-6 pt-0 md:pt-0">
+                    <div class="flex justify-between items-center pt-5">
+                        <button id="dropdownDefaultButton" data-dropdown-toggle="lastDaysdropdown"
+                            data-dropdown-placement="bottom"
+                            class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
+                            type="button">
+                            Last 7 days
+                            <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 4 4 4-4" />
+                            </svg>
+                        </button>
+                        <div id="lastDaysdropdown"
+                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                aria-labelledby="dropdownDefaultButton">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        7 days</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        30 days</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        90 days</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+                <div class="flex justify-between mb-3">
+                    <div class="flex justify-center items-center">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">Payment Chart By
+                            Gender
+                        </h5>
+                    </div>
+                </div>
+
+                <div class="py-6" id="donut-chart"></div>
+
+                <div
+                    class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
+                    <div class="flex justify-between items-center pt-5">
+                        <button id="dropdownDefaultButton" data-dropdown-toggle="lastDaysdropdown"
+                            data-dropdown-placement="bottom"
+                            class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
+                            type="button">
+                            Last 7 days
+                            <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 4 4 4-4" />
+                            </svg>
+                        </button>
+                        <div id="lastDaysdropdown"
+                            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                aria-labelledby="dropdownDefaultButton">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        7 days</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        30 days</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
+                                        90 days</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+                <div class="flex justify-between mb-3">
+                    <div class="flex justify-center items-center">
+                        <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white pe-1">Payment Chart By
+                            Age
+                        </h5>
+                    </div>
+                </div>
+                <div id="line-chart">
+
+                </div>
+            </div>
+
+        </div>
+        <div class="grid grid-cols-3 gap-6 py-10">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                User Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Registrations
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row"
+                                class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                <img class="w-10 h-10 rounded-full" src="/images/members_images/1734689005.jpg"
+                                    alt="Jese image">
+                                <div class="ps-3">
+                                    <div class="text-base font-semibold">Ohene Adjei Darius</div>
+                                    <div class="font-normal flex text-gray-500">
+                                        <span class="flex items-center justify-center text-green-500"> 12<svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 10 14">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4" />
+                                        </svg>
+                                        </span><span class="pl-1">vs last month</span></div>
+                                </div>
+                            </th>
+                            <td class="px-6 py-4 font-bold text-lg text-black">
+                               208901
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </main>
 </div>
